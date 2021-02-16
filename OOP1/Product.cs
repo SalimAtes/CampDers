c8f0ea9fc24342f001a3e7eb5309fb9544a9b3be
salim ateş <salimxas@hotmail.com>
2021-02-16 17:56:46 +0300

    yarım devam edilecek

---
+++ OOP1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{//prop altı 3 nokta = snippet//
+    class Product //entity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsİnStock { get; set; }
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 120;
+            product1.UnitsİnStock = 3;
+
+            Product product2 = new Product {Id=2,CategoryId=5 ,
+                ProductName="sandalye",UnitPrice=100,UnitsİnStock=5  };
+            
+            //case sensitive (duyarlılık)
+            //PascalCase   //camelcase
+            ProductManager productmanager = new ProductManager();
+            productmanager.Add();
+
+
+
+        }
+    }
+}
